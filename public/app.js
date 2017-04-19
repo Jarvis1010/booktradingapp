9535151ec875a734d19c607ae85f72592c3bf5c3
Justin Travis Mair <travis.mair@gmail.com>
2017-04-19 16:13:12 +0000

    adding more classes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,43 @@
-ReactDOM.render(React.createElement(
-	"h1",
-	{ className: "text-center col-xs-12" },
-	"Hello, world!"
-), document.getElementById('example'));
+class BugFilter extends React.Component {
+	render() {
+		return React.createElement(
+			"div",
+			{ className: "col-xs-12 text-center" },
+			"Place Holder Text for Filter"
+		);
+	}
+}
+
+class BugTable extends React.Component {
+	render() {
+		return React.createElement(
+			"div",
+			{ className: "col-xs-12 text-center" },
+			"Place Holder Text for Table"
+		);
+	}
+}
+
+class BugForm extends React.Component {
+	render() {
+		return React.createElement(
+			"div",
+			{ className: "col-xs-12 text-center" },
+			"Place Holder Text for Form"
+		);
+	}
+}
+
+class BugList extends React.Component {
+	render() {
+		return React.createElement(
+			"div",
+			null,
+			React.createElement(BugFilter, null),
+			React.createElement(BugTable, null),
+			React.createElement(BugForm, null)
+		);
+	}
+}
+
+ReactDOM.render(React.createElement(BugList, null), document.getElementById('example'));
--- src/app.js
+++ src/app.js
@@ -1,4 +1,40 @@
+class BugFilter extends React.Component{
+	render(){
+		return(
+			<div className="col-xs-12 text-center">Place Holder Text for Filter</div>	
+		);
+	}
+}
+
+class BugTable extends React.Component{
+	render(){
+		return(
+			<div className="col-xs-12 text-center">Place Holder Text for Table</div>	
+		);
+	}
+}
+
+class BugForm extends React.Component{
+	render(){
+		return(
+			<div className="col-xs-12 text-center">Place Holder Text for Form</div>	
+		);
+	}
+}
+
+class BugList extends React.Component{
+	render(){
+		return(
+			<div>
+				<BugFilter/>
+				<BugTable/>
+				<BugForm/>
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(
-	<h1 className="text-center col-xs-12">Hello, world!</h1>,
+	<BugList/>,
 	document.getElementById('example')
 );
