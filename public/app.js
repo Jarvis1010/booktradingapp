fa3294a3e344d97addbc51a8a6055f36c1bb3c4f
Justin Travis Mair <travis.mair@gmail.com>
2017-04-19 15:56:57 +0000

    transpiling on back end

---
+++ public/app.js
@@ -0,0 +1,5 @@
+ReactDOM.render(React.createElement(
+	"h1",
+	{ className: "text-center col-xs-12" },
+	"Hello, world!"
+), document.getElementById('example'));
---
+++ src/app.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+	<h1 className="text-center col-xs-12">Hello, world!</h1>,
+	document.getElementById('example')
+);
